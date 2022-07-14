6e2eb04a93aca921e148b1244442c22d2485cd8f
ObitoRin <2209246794@qq.com>
2022-07-15 01:12:00 +0800

    map chart6

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,6 +6,7 @@ import { CaseSortChart } from '../components/case-sort-chart2';
 import { BothTrendsChart } from '../components/both-trends-chart3';
 import { TimeTrendsChart } from '../components/time-trends-chart4';
 import { CombatGainsChart } from '../components/combat-gains-chart5';
+import { MapChart } from '../components/map-chart6';
 
 export const Home = () => {
   const year = new Date().getFullYear();
@@ -24,7 +25,9 @@ export const Home = () => {
         <section className="section3">
           <CombatGainsChart />
         </section>
-        <section className="bordered section4"></section>
+        <section className="section4">
+          <MapChart />
+        </section>
         <section className="bordered section5"></section>
       </main>
       <footer>@xk-{year}</footer>

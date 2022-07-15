b40cd485b6ffb4c2b6654652fd5ff982bfef1d8a
ObitoRin <2209246794@qq.com>
2022-07-15 14:48:46 +0800

    right chart 布局

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './home.scss';
-import headerBg from '../images/header.png';
-import { AdministerChart } from '../components/administer-chart1';
-import { CaseSortChart } from '../components/case-sort-chart2';
-import { BothTrendsChart } from '../components/both-trends-chart3';
-import { TimeTrendsChart } from '../components/time-trends-chart4';
-import { CombatGainsChart } from '../components/combat-gains-chart5';
-import { MapChart } from '../components/map-chart6';
-import { AgeChart7 } from '../components/age-chart7';
-import { AgeChart8 } from '../components/age-chart8';
-import { AgeChart9 } from '../components/age-chart9';
+import React from "react";
+import "./home.scss";
+import headerBg from "../images/header.png";
+import { AdministerChart } from "../components/administer-chart1";
+import { CaseSortChart } from "../components/case-sort-chart2";
+import { BothTrendsChart } from "../components/both-trends-chart3";
+import { TimeTrendsChart } from "../components/time-trends-chart4";
+import { CombatGainsChart } from "../components/combat-gains-chart5";
+import { MapChart } from "../components/map-chart6";
+import { AgeChart7 } from "../components/age-chart7";
+import { AgeChart8 } from "../components/age-chart8";
+import { AgeChart9 } from "../components/age-chart9";
 
 export const Home = () => {
   const year = new Date().getFullYear();
@@ -39,7 +39,20 @@ export const Home = () => {
             </div>
           </div>
         </section>
-        <section className="bordered section5"></section>
+        <section className="section5">
+          <div className="bordered row1 case-type">
+            <h2>案发类型统计</h2>
+            <div className="charts"></div>
+          </div>
+          <div className="bordered row2 case-street">
+            <h2>案发街道统计</h2>
+            <div className="charts"></div>
+          </div>
+          <div className="bordered row3 case-mode">
+            <h2>作案手段分析</h2>
+            <div className="charts"></div>
+          </div>
+        </section>
       </main>
       <footer>@xk-{year}</footer>
     </div>
